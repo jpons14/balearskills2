7919416f56fc7ee91c34ccd3badbc47a4f5390e3
josep <jponspons@gmail.com>
2017-02-19 16:26:47 +0100

    CookingTypesController

--- app/Http/Controllers/Backend/CookingTypesController.php
+++ app/Http/Controllers/Backend/CookingTypesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Type_cooking;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Mockery\Matcher\Type;
 
 class CookingTypesController extends Controller
 {
@@ -14,7 +16,10 @@ class CookingTypesController extends Controller
      */
     public function index()
     {
-        //
+        $cookingTypes = Type_cooking::all();
+        return view( 'welcome', [
+            'cookingTypes' => $cookingTypes
+        ] );
     }
 
     /**
@@ -24,62 +29,73 @@ class CookingTypesController extends Controller
      */
     public function create()
     {
-        //
+        return view( 'backend.cookingTypes.create' );
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store( Request $request )
     {
-        //
+        $cookingType = new Type_cooking();
+        $input = $request->all();
+        $cookingType->create( $input );
+        return; // por hacer
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show( $id )
     {
-        //
+        $cookingType = Type_cooking::find( $id );
+        return view( 'backend.cookingTypes.show' )->with( 'cookingType', $cookingType );
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit( $id )
     {
-        //
+        $cookingType = Type_cooking::find( $id );
+        return view( 'backend.cookingTypes.edit', [
+            'cookingType' => $cookingType
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update( Request $request, $id )
     {
-        //
+        $cookingType = Type_cooking::find($id);
+        $input = $request->all();
+        $cookingType->update($input);
+        return; // por hacer
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy( $id )
     {
-        //
+        Type_cooking::destroy($id);
+        return; // por hacer
     }
 }
---
+++ resources/views/backend/cookingTypes/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josep
+ * Date: 19/02/2017
+ * Time: 12:53
+ */
