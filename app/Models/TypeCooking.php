1d340e9b1bae62859bf1dfe29b737e4fcbb191aa
josep <jponspons@gmail.com>
2017-02-19 16:42:51 +0100

    Namespace models

--- app/Models/TypeCooking.php
+++ app/Models/TypeCooking.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Type_cooking extends Model
+class TypeCooking extends Model
 {
     protected $table = 'cooking_types';
     protected $fillable = [
