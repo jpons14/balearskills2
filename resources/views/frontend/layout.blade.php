80beec29fda9c7ceb882cd027fcd48869a086c91
josep <jponspons@gmail.com>
2017-02-23 12:18:51 +0100

    starting frontend

---
+++ resources/views/frontend/layout.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Restaurant</title>
+    <link rel="stylesheet" href="/css/frontend/normalize.css">
+    <link rel="stylesheet" href="/css/frontend/main.css" media="screen" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/frontend/bootstrap.css">
+    <link rel="stylesheet" href="/css/frontend/style-portfolio.css">
+    <link rel="stylesheet" href="/css/frontend/picto-foundry-food.css" />
+    <link rel="stylesheet" href="/css/frontend/jquery-ui.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/frontend/font-awesome.min.css" rel="stylesheet">
+    <link rel="icon" href="favicon-1.ico" type="image/x-icon">
+</head>
+
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="row">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">BalearSkills</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav main-nav  clear navbar-right ">
+                    <li><a class="navactive color_animation" href="#top">WELCOME</a></li>
+                    <li><a class="color_animation" href="#story">RESTAURANTS</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div id="top" class="starter_container bg">
+    <div class="follow_container">
+        <div class="col-md-6 col-md-offset-3">
+            <h2 class="top-title"> Restaurant</h2>
+            <h2 class="white second-title">" Best in the city "</h2>
+            <hr>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="/js/jquery-1.10.2.min.js"> </script>
+<script type="text/javascript" src="/js/bootstrap.min.js" ></script>
+<script type="text/javascript" src="/js/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="/js/jquery.mixitup.min.js" ></script>
+<script type="text/javascript" src="/js/main.js" ></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,16 @@
 |
 */
 
-Route::get( '/', function (){
-    return view( 'welcome' );
-} );
-//tests
-Route::get('/lang/{lan}', array('uses' => 'Controller@setLang', 'as' => 'lang'));
 
 Route::resource('establishments', 'Backend\EstablishmentController');
 Route::resource('comments', 'Backend\CommentsController');
 Route::resource('cookingTypes', 'Backend\CookingTypesController');
 Route::resource('photos', 'Backend\PhotosController');
+
+
+// Frontend
+Route::get('/', 'Frontend\FrontendController@index');
+
 
 Auth::routes();
 
